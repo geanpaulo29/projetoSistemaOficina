9ecff0800fcde63532b3942383d549d6d1aa75b8
geanpaulo29 <geanp.antonio@gmail.com>
2025-03-06 19:25:26 -0300

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,7 +35,10 @@
         <nav class="navbar navbar-expand-lg navbar-light navbar-custom fixed-top">
             <div class="container-fluid">
                 <!-- Botão para voltar à home -->
-                <a class="navbar-brand" href="{{ route('home') }}">Home</a>
+                <a class="navbar-brand" href="{{ route('home') }}"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
+  <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
+</svg>Home</a>
 
                 <!-- Botões da Home -->
                 <div class="navbar-nav me-auto">
